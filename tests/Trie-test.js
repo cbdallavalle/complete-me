d3565eab3ea0e9831c61b0ab75e12225652add62
Casey Dallavalle <cbdallavalle@gmail.com>
2017-12-20 15:07:53 -0700

    Add delete function In the delete function, we want to delete the words from the suggested array but not delete the entire word from the dictionary. The delete function turns the is completed tag off so that the word will not be suggested, but still exists in the trie. Also, add testing for delete and sad path

--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -3,7 +3,11 @@ import Node from '../lib/Node';
 import Trie from '../lib/Trie';
 import fs from 'fs';
 
-describe.only('Trie', function() {
+const text = "/usr/share/dict/words";
+const dictionary = fs.readFileSync(text).toString().trim().split('\n');
+
+
+describe('Trie', function() {
   let trie;
 
   beforeEach(() => {
@@ -16,69 +20,140 @@ describe.only('Trie', function() {
     expect(trie).to.exist
   });
 
-  it('should be have a default root of null', () => {
-    expect(trie.root.data).to.equal(null);
-  });
+  describe('insert', () => {
 
-  it('should insert a word', () => {
-    trie.insert('ardvark');
-    expect(trie.root.children.a.data).to.equal('a');
-    expect(trie.root.children.a.children.r.data).to.equal('r')
-    expect(trie.root.children.a.children.r.children.d.data).to.equal('d');
-    expect(trie.root.children.a.children.r.children.d.children.v.children.a.children.r.children.k.data).to.equal('k');
-    expect(trie.root.children.a.children.r.children.d.children.v.children.a.children.r.children.k.isCompleted).to.equal(true);
-  });
+    it('should insert a word', () => {
+      trie.insert('ardvark');
+      expect(trie.root.children.a.data).to.equal('a');
+      expect(trie.root.children.a.children.r.data).to.equal('r')
+      expect(trie.root.children.a.children.r.children.d.data).to.equal('d');
+      expect(trie.root.children.a.children.r.children.d.children.v.children.a.children.r.children.k.data).to.equal('k');
+      expect(trie.root.children.a.children.r.children.d.children.v.children.a.children.r.children.k.isCompleted).to.equal(true);
+    });
 
-  it('should insert multiple words and increase completed count', () => {
-    trie.insert('platypus');
-    trie.insert('capybara');
-// console.log( JSON.stringify(trie, null, 4) );
-    expect(trie.root.children.p.children.l.children.a.children.t.children.y.children.p.children.u.children.s.data).to.equal('s');
-    expect(trie.root.children.p.children.l.children.a.children.t.children.y.children.p.children.u.children.s.isCompleted).to.equal(true);
-  
-    expect(trie.root.children.c.children.a.children.p.children.y.children.b.children.a.children.r.children.a.data).to.equal('a');
-    expect(trie.root.children.c.children.a.children.p.children.y.children.b.children.a.children.r.children.a.isCompleted).to.equal(true);
-  
-    expect(trie.completed).to.be.equal(2);
+    it('should insert multiple words, flag isComplete property, increase completed count', () => {
+      trie.insert('platypus');
+      trie.insert('capybara');
+      expect(trie.root.children.p.children.l.children.a.children.t.children.y.children.p.children.u.children.s.data).to.equal('s');
+      expect(trie.root.children.p.children.l.children.a.children.t.children.y.children.p.children.u.children.s.isCompleted).to.equal(true);
+      expect(trie.root.children.p.children.l.children.a.children.t.children.y.children.p.children.u.children.s.isCompleted).to.equal(true);
 
-  });
-
-  it('should insert several words that start with the same letter and not create new nodes', () => {
-    trie.insert('armadillo');
-    trie.insert('ardvark');
-    trie.insert('anteater');
-    expect(trie.root.children.a.children.r.data).to.equal('r');
-    expect(trie.root.children.a.children.r.children.m.data).to.equal('m');
-    expect(trie.root.children.a.children.r.children.d.data).to.equal('d');
-    expect(trie.root.children.a.children.n.data).to.equal('n');
-  });
-
-  it('should suggest an array of one word that match the word you put in', () => {
-    trie.insert('pizza');
-    expect(trie.suggest('pi')).to.deep.equal(["pizza"]);
-  });
-
-  it('should suggest an array of words that match the words you are putting in', () => {
-    trie.insert('manatee');
-    trie.insert('mangabey');
-    trie.insert('mantaray');
-    // console.log( JSON.stringify(trie, null, 4) );
-    expect(trie.suggest('ma')).to.deep.equal(["manatee", "mangabey", "mantaray"]);
-  });
-
-  it('should take in a massive amount of words', () => {
-    const text = "/usr/share/dict/words"
-    const dictionary = fs.readFileSync(text).toString().trim().split('\n')
-    const completion = new Trie()
-
-    completion.populate(dictionary)
+      expect(trie.root.children.c.children.a.children.p.children.y.children.b.children.a.children.r.children.a.data).to.equal('a');
+      expect(trie.root.children.c.children.a.children.p.children.y.children.b.children.a.children.r.children.a.isCompleted).to.equal(true);
     
-    expect(completion.count()).to.equal(235886);
-    expect(completion.suggest('piz')).to.deep.equal(["pize", "pizza", "pizzeria", "pizzicato", "pizzle"]);
+      expect(trie.completed).to.be.equal(2);
+
+    });
+
+    it('should insert several words that start with the same letter and not create new nodes', () => {
+      trie.insert('armadillo');
+      trie.insert('ardvark');
+      trie.insert('anteater');
+      expect(trie.root.children.a.children.r.data).to.equal('r');
+      expect(trie.root.children.a.children.r.children.m.data).to.equal('m');
+      expect(trie.root.children.a.children.r.children.d.data).to.equal('d');
+      expect(trie.root.children.a.children.n.data).to.equal('n');
+      expect(trie.root.children.a.children.n.children.t.children.e.children.a.children.t.children.e.children.r.isCompleted).to.equal(true);
+    });
+
+  });
+
+  describe('suggest', () => {
+    it('should suggest an array of one word that match the word you put in', () => {
+      trie.insert('pizza');
+      expect(trie.suggest('pi')).to.deep.equal(["pizza"]);
+    });
+
+    it('should suggest an array of words that match the words you are putting in', () => {
+      trie.insert('manatee');
+      trie.insert('mangabey');
+      trie.insert('mantaray');
+      expect(trie.suggest('ma')).to.deep.equal(["manatee", "mangabey", "mantaray"]);
+    });
+
+    it('should not suggest a word that does not start with the snippet', () => {
+      trie.insert('manatee');
+      trie.insert('mangabey');
+      trie.insert('mantaray');
+      trie.insert('macaw')
+      expect(trie.suggest('man')).to.deep.equal(["manatee", "mangabey", "mantaray"]);
+    });
+
+  });
+
+  describe('populate', () => {
+
+    it('should take in a massive amount of words', () => {
+      const completion = new Trie();
+
+      completion.populate(dictionary);
+      
+      expect(completion.count()).to.equal(235886);
+      expect(completion.suggest('piz')).to.deep.equal(["pize", "pizza", "pizzeria", "pizzicato", "pizzle"]);
+    })
+
+    //sad path
+      //a random string of words
+      //other data types besides a string
+      //what if someone asks for a suggestion but there is none
+  })
+
+  describe('select' , () => {
+
+    it('should exist', () => {
+      expect(trie.select).to.exist;
+    })
+
+    it('should select stuff', () => {
+      trie.populate(dictionary);
+
+      trie.select('yak');
+
+      expect(trie.root.children.y.children.a.children.k.timesSelected).to.equal(1);
+
+      trie.select('pizza');
+      trie.select('pizza');
+      trie.select('pizzeria');
+      trie.select('pize');
+
+      expect(trie.sort(["pize", "pizza", "pizzeria", "pizzicato", "pizzle"])).to.deep.equal(["pizza", "pize", "pizzeria", "pizzicato", "pizzle"]);
+
+    })
+  });
+
+  describe('delete', () => {
+
+    it('should exist', () => {
+      expect(trie.delete).to.exist;
+    })
+
+    it('should delete a word that the user does not want', () => {
+      trie.populate(dictionary);
+
+      trie.delete('pizzle');
+      trie.select('pizzle')
+
+      expect(trie.suggest('piz')).to.deep.equal(["pize", "pizza", "pizzeria", "pizzicato"]);
+      expect(trie.root.children.p.children.i.children.z.children.z.children.l.children.e.isCompleted).to.equal(false);
+    });
+  })
+
+  describe('sad path', () => {
+
+    it('should not be able to take a data type that is not a string', () => {
+      expect(trie.insert(12312)).to.equal(null);
+      expect(trie.insert(['pangolin', 'slow loris', 'axoltol'])).to.equal(null);
+      expect(trie.insert({'bearded vulture': 'bird'})).to.equal(null);
+    });
+
+    it('should return null if selecting a something that is not a word', () => {
+      trie.populate(dictionary);
+
+      expect(trie.select('adsaweasdasdf')).to.equal(null);
+    });
+
   })
 
 
 
-
-
-})
+});
